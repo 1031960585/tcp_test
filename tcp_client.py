ca55de2855cb78045c2b1fed5593ca25682261ae
lvrunze <1031960585@qq.com>
2022-03-16 19:06:50 +0800

    基本功能

--- tcp_client.py
+++ tcp_client.py
@@ -1,12 +1,25 @@
-import PySide2
-from PySide2.QtWidgets import QApplication, QMessageBox,QFileDialog
+# import PySide2
+from PySide2.QtWidgets import QApplication, QOpenGLWidget,QFileDialog
 from PySide2.QtUiTools import QUiLoader
-import os
-import sys
-import PIL.Image as pil
+from PySide2.QtCore import Signal,QObject
+# import os
+# import sys
 import socket
-from io import BytesIO
 import time
+from threading import Thread
+import cv2
+
+
+# 自定义信号源对象类型，一定要继承自 QObject
+class MySignals(QObject):
+
+    # 定义一种信号，两个参数 类型分别是： QTextBrowser 和 字符串
+    # 调用 emit方法 发信号时，传入参数 必须是这里指定的 参数类型
+    # text_print = Signal(QTextBrowser,str)
+
+    # 还可以定义其他种类的信号
+    update_table = Signal(str)
+
 
 class Stats:
     def __init__(self):
@@ -17,9 +30,34 @@ class Stats:
         # 比如 self.ui.button , self.ui.textEdit
         self.ui = QUiLoader().load('tcp_client.ui')
         self.connectable = False
+        self.sending = False
+        self.terminate = False
+        self.Obstacles = [self.ui.Obstacle_1,
+            self.ui.Obstacle_2,
+            self.ui.Obstacle_3,
+            self.ui.Obstacle_4,
+            self.ui.Obstacle_5,
+            self.ui.Obstacle_6,
+            self.ui.Obstacle_7,
+            self.ui.Obstacle_8,
+            self.ui.Obstacle_9,
+            self.ui.Obstacle_10]
+        self.ui.Loop_send.setEnabled(False)
         self.ui.Connectable.clicked.connect(self.connect)
         self.ui.Video_search.clicked.connect(self.openFileNameDialog)
+        self.ui.Loop_send.clicked.connect(self.send)
+        self.ui.buttonGroup.buttonClicked.connect(self.choose1or2)
         
+
+    def choose1or2(self):
+        if self.ui.buttonGroup.checkedButton().text()=="图像(一)":
+            for Obstacle in self.Obstacles:
+                Obstacle.setEnabled(True)
+        elif self.ui.buttonGroup.checkedButton().text()=="图像(二)":
+            for Obstacle in self.Obstacles:
+                Obstacle.setEnabled(False)        
+            
+
     def connect(self):
         if self.connectable == False:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,53 +66,69 @@ class Stats:
             self.sock.connect((HOST,PORT))
             self.connectable = True
             self.ui.Connectable.setText("断开")
+            self.ui.Loop_send.setEnabled(True)
         else:
             self.sock.close()
             self.connectable = False
             self.ui.Connectable.setText("连接")
+            self.ui.Loop_send.setEnabled(False)
 
     def openFileNameDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        self.fileName, _ = QFileDialog.getOpenFileName(self.ui, "QFileDialog.getOpenFileName()", "","Video files (*.mp4 *.rmvb *.avi *.rm)", options=options)
+        self.fileName, _ = QFileDialog.getOpenFileName(self.ui, \
+            "QFileDialog.getOpenFileName()", "","Video files (*.mp4 *.rmvb *.avi *.rm)", options=options)
         self.ui.Video_path.setText(self.fileName)
 
     def send(self):
-        if self.QButtonGroup.checkedButton().text()=="图像(一)":
-            
-            head = bytearray(b'\xaa\xfe')
-            end = bytearray(b'\x55\xee')
-            Obstacle = 12312.15435
-            int_Obstacle = int(Obstacle*100)
+        def threadFunc():
+            caps = cv2.VideoCapture(self.fileName)
+            while True:
+                if self.terminate:
+                    self.terminate = False
+                    break
+                ret, frame = caps.read()
+                head = bytearray(b'\xaa\xfe')
+                end = bytearray(b'\x55\xee')            
+                if ret:
+                    _, encoded_image = cv2.imencode(".jpg", frame)
+                    if self.ui.buttonGroup.checkedButton().text()=="图像(一)":
+                        Obstacles = bytearray(b'')
+                        for obs in self.Obstacles:
+                            Obstacles += bytearray(int(float(obs.text())*100).to_bytes(4, byteorder='big',signed=True))
+                        content = bytearray(b'\x01')+encoded_image.tobytes()+bytearray(b'\x02')+Obstacles
+                        
+                        datalen = len(content)
+                        datalen = bytearray(datalen.to_bytes(4, byteorder='big'))
 
-            # PIL转二进制
-            # im.tobytes()
-            bytesIO = BytesIO()
-            im.save(bytesIO, format='JPEG')
-            content = bytearray(b'\x01')+bytesIO.getvalue()+bytearray(b'\x01')+bytearray(int_Obstacle.to_bytes(4, byteorder='little'))*10
-
-            datalen = len(content)
-            datalen = bytearray(datalen.to_bytes(4, byteorder='little'))
-
-            alldata = head + datalen + content + end
-            self.sock.sendall(alldata)
-            # time.sleep(2)
-
+                        alldata = head + datalen + content + end
+                    elif self.ui.buttonGroup.checkedButton().text()=="图像(二)":
+                        content = bytearray(b'\x03')+encoded_image.tobytes()
+                        datalen = len(content)
+                        datalen = bytearray(datalen.to_bytes(4, byteorder='big'))
+                        alldata = head + datalen + content + end                    
+                self.sock.sendall(alldata)
+                time.sleep(0.001*int(self.ui.Sleep.text()))
+        if not self.sending:
+            self.thread = Thread(target = threadFunc)
+            self.thread.start()
+            self.sending = True
+            self.ui.Loop_send.setText("停止")
+            self.ui.Connectable.setEnabled(False)
         else:
-            pass
+            self.terminate = True
+            self.sending = False
+            self.ui.Loop_send.setText("发送")
+            self.ui.Connectable.setEnabled(True)
 
 
-        QMessageBox.about(self.ui,
-                    '统计结果',
-                    f'''薪资20000 以上的有：\n{salary_above_20k}
-                    \n薪资20000 以下的有：\n{salary_below_20k}'''
-                    )
 
 if __name__=="__main__":
-    dirname = os.path.dirname(PySide2.__file__)
-    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
-    app = QApplication(sys.argv)
+    # dirname = os.path.dirname(PySide2.__file__)
+    # plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    # os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+    # print(plugin_path)
+    app = QApplication([])
     stats = Stats()
     stats.ui.show()
     app.exec_()
